c2b2a09300d052431cb4fa44bc19d2b916d8fa26
Maximilian Heeß <max-heess@gmx.de>
2025-04-17 14:15:23 +0200

    Add files via upload

---
+++ Python_Basics/Lv 02 Variables/Project_Band_Name_Generator/Band_Name_Generator_blank.py
@@ -0,0 +1,11 @@
+# TODO 0. Erstellen Sie ein PAP für das folgende Programmierprojekt.
+
+# TODO 1. Erstellen Sie eine Begrüßung für Ihr Programm
+
+# TODO 2. Fragen Sie den Benutzenden, in welcher Stadt er oder sie aufgewachsen ist.
+
+# TODO 3. Fragen Sie den Benutzenden, nach seinem oder ihrem Lieblingstier.
+
+# TODO 4. Kombinieren Sie den Namen der Stadt, sowie den Namen des Lieblingstieres zu einem Band Namen.
+
+# TODO 5. Achten Sie darauf, dass der Curser anschließend in eine neue Zeile springt.
